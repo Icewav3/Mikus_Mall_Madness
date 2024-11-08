cdc7dbf141fd06c384d9c5a57ee6a8264ea6d448
Alex Quennelle <bluephoenixcg@gmail.com>
2024-11-08 17:15:10 -0600

    Test

--- Assets/blah.cs
+++ Assets/blah.cs
@@ -3,7 +3,7 @@
 public class blah : MonoBehaviour
 {
 	private int an_integer = 5;
-	// Start is called before the first frame update aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+	// Start is called before the first frame update
 	void Start ( ){
 		var A=0;
         if(an_integer>=A){
