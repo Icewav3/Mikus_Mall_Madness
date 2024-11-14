0b7576f61ba5337d6bc5e06530170e52ce223d53
caterunions <cate.runions@gmail.com>
2024-11-14 11:49:32 -0600

    Add more edge case handling to character stat accessors

--- Assets/Scripts/Combat/Character/Character.cs
+++ Assets/Scripts/Combat/Character/Character.cs
@@ -29,6 +29,9 @@ public class Character : MonoBehaviour
 	private int _currentHealth;
 	public int CurrentHealth => _currentHealth;
 
+	private int _currentStamina;
+	public int CurrentStamina => _currentStamina;
+
 	public int MaxHealth
 	{
 		get { return Mathf.FloorToInt(ApplyStatBoosts(CharacterBase.BaseHealth, StatTypes.MaxHealth)); }
@@ -103,14 +106,26 @@ public class Character : MonoBehaviour
 	{
 		if (heal <= 0) return;
 
-		_currentHealth += heal;
+		int appliedHeal = heal;
 
-		OnHeal?.Invoke(this, heal);
+		if(_currentHealth + heal > MaxHealth)
+		{
+			appliedHeal = MaxHealth - _currentHealth;
+			_currentHealth = MaxHealth;
+		}
+		else
+		{
+			_currentHealth += heal;
+		}
+
+		OnHeal?.Invoke(this, appliedHeal);
 	}
 
 	public void DepleteStamina(int stamina)
 	{
 		if (stamina <= 0) return;
+
+		_currentStamina -= stamina;
 
 		OnStaminaDeplete?.Invoke(this, stamina);
 	}
@@ -119,7 +134,19 @@ public class Character : MonoBehaviour
 	{
 		if (stamina <= 0) return;
 
-		OnStaminaGain?.Invoke(this, stamina);
+		int gainedStamina = stamina;
+
+		if(_currentStamina + stamina > MaxStamina)
+		{
+			gainedStamina = MaxStamina - _currentStamina;
+			_currentStamina = MaxStamina;
+		}
+		else
+		{
+			_currentStamina += stamina;
+		}
+
+		OnStaminaGain?.Invoke(this, gainedStamina);
 	}
 	#endregion
 }
