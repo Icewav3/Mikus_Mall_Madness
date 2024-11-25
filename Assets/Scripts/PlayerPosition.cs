7e357bf1e545a213420893285a521d9a113e71cc
Cole Dorman <coledorman12@gmail.com>
2024-11-25 12:59:21 -0600

    Added scriptable objects

---
+++ Assets/Scripts/PlayerPosition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerPosition", menuName = "PlayerPosition", order = 0)]
+public class PlayerPosition : ScriptableObject
+{
+	private Vector2 _playerPosition;
+	public Vector2 Position => _playerPosition;
+
+	public void SetPosition(Vector2 position)
+	{
+		_playerPosition = position;
+	}
+}
