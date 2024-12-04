8edecddd787ef154f593b2843e9dbfbe8dad4f09
xyeFox <ibixorblx@gmail.com>
2024-12-03 21:07:28 -0600

    Luka Final, Character Prefab Update

--- Assets/Art Files/Characters/Luka/Animation/Luka_VisualsController.cs
+++ Assets/Art Files/Characters/Luka/Animation/Luka_VisualsController.cs
@@ -9,10 +9,22 @@ public class Luka_VisualsController : MonoBehaviour
 	private ParticleSystem _ultInitialSpin;
 	[SerializeField]
 	private ParticleSystem _ultBigSpin;
+	[SerializeField]
+	private ParticleSystem _atkSlash;
 
 	public void ultPhase1Spin()
 	{
 		_ultInitialSpin.Emit(3);
+	}
+
+	public void ultPhase2Spin()
+	{
+		_ultBigSpin.Emit(5);
+	}
+
+	public void atkSlash()
+	{
+		_atkSlash.Play();
 	}
 
 	public void changeSortingLayerUP()
